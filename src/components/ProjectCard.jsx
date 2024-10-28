6a82c8df054f2326e9c237518ea0bbb7a1247ce1
Drakvlaa <Filip.sobczuk@o2.pl>
2024-10-28 22:28:49 +0100

    box shadow on project hover

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -13,8 +13,8 @@ export default function ProjectCard( props ) {
     'weave': weave,
   }
   return (
-    <div className="bg-white/10 w-80 h-96 rounded-2xl grid grid-rows-2 grid-cols-1">
-      <img src={props.imgPath in images ? images[props.imgPath] : images['default']} alt="card" className="h-full w-full rounded-t-2xl"/>
+    <div className="bg-white/10 w-80 h-96 rounded-2xl grid grid-rows-2 grid-cols-1 duration-200 ease-in hover:scale-110 hover:shadow-purple-800 shadow-2xl">
+      <img src={props.imgPath in images ? images[props.imgPath] : images['default']} alt="card" className="h-full w-full rounded-t-2xl image-full"/>
       <div className="w-full h-full p-2 mt-3">
         <center><a href={props.link} target="_blank" className="text-2xl font-semibold">{props.title ? props.title : "Project"}</a><br/>
           <span className="font-ubuntu font-medium">{props.text ? props.text : "description"}</span><br/>
